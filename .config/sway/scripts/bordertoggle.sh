c0200ddfd83f7687907fed3852998c18917575e2
FailedEx <thereisanimposteramongus@gmail.com>
2024-02-24 19:13:23 +1300

    screenshot touch button

--- .config/sway/scripts/bordertoggle.sh
+++ .config/sway/scripts/bordertoggle.sh
@@ -5,8 +5,10 @@
 keyboard="1:1:AT_Translated_Set_2_keyboard"
 
 case "$1" in
-    tablet)    new_border_mode="normal" && swaymsg input $keyboard events disabled && swaymsg default_border normal;;
-    *) new_border_mode="pixel" && swaymsg input $keyboard events enabled && swaymsg default_border pixel 2;;
+    # tablet)    new_border_mode="normal" && swaymsg input $keyboard events disabled && swaymsg default_border normal;;
+    # *) new_border_mode="pixel" && swaymsg input $keyboard events enabled && swaymsg default_border pixel 2;;
+    tablet)    swaymsg input $keyboard events disabled && swaymsg "[app_id='.*']" inhibit_idle open;;
+    *) swaymsg input $keyboard events enabled && swaymsg "[app_id='.*']" inhibit_idle none;;
 esac
 
 swaymsg "[workspace=\"^[1-5]$\"] border $new_border_mode"
